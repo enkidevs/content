22a87ee2d7c57d398e926605844d9667d5ac2023
Stefano Bourscheid <stefano.bourscheid@gmail.com>
2017-02-19 17:22:47 -0300

    Improved the JSON representation of the insight. :bowtie:

--- tests/parser.js
+++ tests/parser.js
@@ -55,20 +55,142 @@ function toMarkdownLink (link) {
   return text
 }
 
+function setPropertiesData (parsedJson, lines) {
+  const newJson = {}
+  Object.keys(parsedJson).forEach(key => {
+    const content = parsedJson[key]
+
+    // Things are getting a + 1 there because the index starts from zero
+    if (Array.isArray(content)) {
+      newJson[key] = {
+        value: content.map(listValue => ({
+          value: listValue,
+          line: lines.indexOf(lines.find(line => line.indexOf(listValue) >= 0)) + 1,
+          column: lines.find(line => line.indexOf(listValue) >= 0).indexOf(listValue) + 1,
+          tip: key + ':',
+          length: listValue.length
+        })),
+        line: lines.indexOf(lines.find(line => line.indexOf(key + ':') >= 0)) + 1,
+        column: 1,
+        tip: key + ':',
+        length: content.reduce((prev, next) => prev + next.length, 0)
+      }
+    } else {
+      newJson[key] = {
+        value: content,
+        line: lines.indexOf(lines.find(line => line.indexOf(key + ':') >= 0)) + 1,
+        column: (key + ':').length + 1,
+        tip: key + ':',
+        length: content.toString().length
+      }
+    }
+  })
+
+  return newJson
+}
+
+function getReferenceByProp (prop) {
+  let regex
+  let string
+  switch (prop) {
+    case 'content':
+      regex = /\n##\sContent\n*/
+      string = '## Content'
+      break
+    case 'practiceQuestion':
+      regex = /\n##\sPractice\n*/
+      string = '## Practice'
+      break
+    case 'reviseQuestion':
+      regex = /\n##\sRevision\n*/
+      string = '## Revision'
+      break
+    case 'gameContent':
+      regex = /\n##\sGame\sContent\n*/
+      string = '## Game Content'
+      break
+    case 'footnotes':
+      regex = /\n##\sFootnotes\n*/
+      string = '## Footnotes'
+  }
+
+  return {regex, string}
+}
+
+function fillNullProperties (parsedProperties) {
+  const completeProperties = [
+    'headline',
+    'author',
+    'levels',
+    'type',
+    'category',
+    'links',
+    'notes',
+    'parent',
+    'tags',
+    'inAlgoPool',
+    'content',
+    'gameContent',
+    'practiceQuestion',
+    'reviseQuestion',
+    'footnotes'
+  ]
+
+  completeProperties.forEach(prop => {
+    const found = Object.keys(parsedProperties).some(parsedProp => {
+      if (parsedProp === prop && parsedProperties[parsedProp]) {
+        return true
+      }
+    })
+
+    if (!found) {
+      const tip = getReferenceByProp(prop)
+      parsedProperties[prop] = {
+        value: null,
+        line: null,
+        column: null,
+        tip: tip.string ? tip.string : prop + ':',
+        length: 0
+      }
+    }
+  })
+
+  return parsedProperties
+}
+
 export function parse (string = '') {
-  let sections = string.split('---')
+  const sections = string.split('---')
+  const lines = string.split('\n')
 
   const meta = sections.shift()
 
   let [headline, ...props] = meta.split('\n')
-  headline = headline.replace(/^#\s/, '')
 
+  // Extracts only the headline (removes # and whitespaces from the original .md)
+  const rawHeadline = headline.replace(/^#\s/, '')
+  headline = {
+    value: rawHeadline,
+    line: 1,
+    column: headline.indexOf(rawHeadline) + 1,
+    tip: '# My Headline (First line in .md)',
+    length: rawHeadline ? rawHeadline.length : null
+  }
+
+  // Parses the string into yaml then json
   const yamlString = props.join('\n').replace(/^\s+|\s+$/g, '')
+  // The next replace is to replace (author:stefano) for (author: stefano)
+  // since it would crash the yaml
+  let attributes = yaml.safeLoad(yamlString.replace(/\b[:]\b/g, ': ')) || {}
 
-  const attributes = yaml.safeLoad(yamlString) || {}
-
-  if (attributes.links) {
-    attributes.links = attributes.links.map(getMarkdownLink)
+  // Sets extra info for better error handling
+  attributes = setPropertiesData(attributes, lines)
+  // Insert 'links' array
+  if (attributes.links && attributes.links.value) {
+    attributes.links.value = attributes.links.value.map(link => {
+      const linkProps = link
+      linkProps.value = getMarkdownLink(linkProps.value)
+      return linkProps
+    })
   }
 
   const parsed = {
@@ -78,52 +200,60 @@ export function parse (string = '') {
     reviseQuestion: undefined,
     footnotes: undefined
   }
-  let last
 
+  // Parse the content of the .md file
+  let lastProp
   sections.forEach(section => {
-    if (section.indexOf('\n## Content\n\n') === 0) {
-      last = 'content'
-      parsed[last] = section.replace(/^\n##\sContent\n\n/, '')
-    } else if (section.indexOf('\n## Practice\n\n') === 0) {
-      last = 'practiceQuestion'
-      parsed[last] = section.replace(/^\n##\sPractice\n\n/, '')
-    } else if (section.indexOf('\n## Revision\n\n') === 0) {
-      last = 'reviseQuestion'
-      parsed[last] = section.replace(/^\n##\sRevision\n\n/, '')
-    } else if (section.indexOf('\n## Game Content\n\n') === 0) {
-      last = 'gameContent'
-      parsed[last] = section.replace(/^\n##\sGame\sContent\n\n/, '')
-    } else if (section.indexOf('\n## Footnotes\n\n') === 0) {
-      last = 'footnotes'
-      parsed[last] = section.replace(/^\n##\sFootnotes\n\n/, '')
-    } else {
-      parsed[last] += '---' + section
+    const foundSection = Object.keys(parsed).some(prop => {
+      const {regex, string} = getReferenceByProp(prop)
+
+      if (regex.test(section)) {
+        parsed[prop] = {
+          value: section.replace(regex, '').trim(),
+          line: lines.indexOf(lines.find(l => l.indexOf(string) >= 0)) + 2,
+          column: 1, // These sections always start at the first column
+          tip: string,
+          length: section.replace(regex, '').length
+        }
+        lastProp = prop
+        return true
+      }
+    })
+    // If this section does not start with one property, it belongs to the previous
+    if (!foundSection) {
+      parsed[lastProp].value += '---' + section
+      parsed[lastProp].length += ('---' + section).length
     }
   })
 
-  Object.keys(parsed).forEach(k => {
-    if (parsed[k]) {
-      parsed[k] = parsed[k].trim()
-    }
-  })
-
-  return {...attributes, headline, ...parsed}
+  return fillNullProperties({...attributes, headline, ...parsed})
 }
 
 export function generate (insight) {
   const {headline, content, practiceQuestion, reviseQuestion, gameContent, footnotes, ...attributes} = insight
 
-  if (attributes.links) {
-    attributes.links = attributes.links.map(toMarkdownLink)
-  }
+  const simplifiedJson = {}
+  Object.keys(attributes).forEach(prop => {
+    const attribute = attributes[prop]
+    if (Array.isArray(attribute.value)) {
+      simplifiedJson[prop] = attribute.value.map(v => v.value)
+    } else {
+      simplifiedJson[prop] = attribute.value
+    }
+  })
 
-  return `# ${headline}\n` +
-    yaml.safeDump(attributes, {skipInvalid: true, newline: '\n\n'}) +
-    '\n---\n## Content\n\n' + (content || '').trim() +
-    (gameContent ? ('\n\n---\n## Game Content\n\n' + gameContent.trim()) : '') +
-    (practiceQuestion ? ('\n\n---\n## Practice\n\n' + practiceQuestion.trim()) : '') +
-    (reviseQuestion ? ('\n\n---\n## Revision\n\n' + reviseQuestion.trim()) : '') +
-    (footnotes ? ('\n\n---\n## Footnotes\n\n' + footnotes.trim()) : '')
+  if (simplifiedJson.links) {
+    simplifiedJson.links = simplifiedJson.links.map(toMarkdownLink)
+  }
+  simplifiedJson.inAlgoPool = false
+
+  return `# ${headline.value}\n` +
+    yaml.safeDump(simplifiedJson, {skipInvalid: true, newline: '\n\n'}) +
+    '\n---\n## Content\n\n' + (content.value || '').trim() +
+    (gameContent.value ? ('\n\n---\n## Game Content\n\n' + gameContent.value.trim()) : '') +
+    (practiceQuestion.value ? ('\n\n---\n## Practice\n\n' + practiceQuestion.value.trim()) : '') +
+    (reviseQuestion.value ? ('\n\n---\n## Revision\n\n' + reviseQuestion.value.trim()) : '') +
+    (footnotes.value ? ('\n\n---\n## Footnotes\n\n' + footnotes.value.trim()) : '')
 }
 
 export function encodePath (_path = '') {

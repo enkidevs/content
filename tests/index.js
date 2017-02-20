f623f1042f74ba64f3c3e09d95a4a4b68bda6778
Mathieu Dutour <mathieu@dutour.me>
2017-02-20 14:40:26 +0000

    keep rules in dir

--- tests/index.js
+++ tests/index.js
@@ -2,12 +2,10 @@ import * as pathUtils from './pathUtils'
 import * as CONSTANTS from './constants'
 import * as parser from './parser'
 import questionParser from './questionParser'
-import rules from './rules'
 
 module.exports = {
   ...pathUtils,
   ...CONSTANTS,
   ...parser,
-  questionParser,
-  rules
+  questionParser
 }

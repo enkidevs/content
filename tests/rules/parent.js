3efc91cf7675eae4613c49b26913981901d0f39c
Mathieu Dutour <mathieu@dutour.me>
2017-02-23 10:35:12 +0000

    handle parents in the same PR

--- tests/rules/parent.js
+++ tests/rules/parent.js
@@ -1,8 +1,15 @@
+import { extractNames } from '../pathUtils'
+
 export const context = 'parent'
 export const description = 'Parent insight must exist in database'
 export const link = 'https://enkidevs.github.io/guidelines/Insights-guidelines.html#parent'
 
-export default function ({parent}, {db}) {
+export default function ({parent}, {db, otherFiles, team}) {
   if (typeof parent === 'undefined') return true
+  if (otherFiles && otherFiles.length) {
+    if (otherFiles.map(({filename}) => extractNames(filename, team).slug).indexOf(parent) !== -1) {
+      return true
+    }
+  }
   return db.insight.findOne({slug: parent}, '_id').lean()
 }

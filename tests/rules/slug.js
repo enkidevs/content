ff7ef18e47185441f5c0add54fcad6487d05ee14
Mathieu Dutour <mathieu@dutour.me>
2017-02-28 11:20:09 +0000

    add default value

--- tests/rules/slug.js
+++ tests/rules/slug.js
@@ -5,7 +5,7 @@ export const description = 'Slug shouldn\'t already exist'
 
 export default function (file, {db, rawFile, team, status}) {
   const {slug} = extractNames(rawFile.filename, team)
-  const previous = rawFile.previous_filename || (status === 'modified' && rawFile.filename) // eslint-disable-line camelcase
+  const previous = rawFile.previous_filename || (status === 'modified' && rawFile.filename) || '' // eslint-disable-line camelcase
   const {slug: previousSlug} = extractNames(previous, team)
   if (!previousSlug || previousSlug !== slug) {
     return db.Insight.findOne({slug}, '_id').lean().then(i => !i)

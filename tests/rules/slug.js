43c112434be1af66eea2ac3e2b8b66237942228d
Mathieu Dutour <mathieu@dutour.me>
2017-03-01 12:49:16 +0000

    fix status access

--- tests/rules/slug.js
+++ tests/rules/slug.js
@@ -3,9 +3,9 @@ import { extractNames } from '../pathUtils'
 export const context = 'slug'
 export const description = 'Slug shouldn\'t already exist'
 
-export default function (file, {db, rawFile, team, status}) {
+export default function (file, {db, rawFile, team}) {
   const {slug} = extractNames(rawFile.filename, team)
-  const previous = rawFile.previous_filename || (status === 'modified' && rawFile.filename) || '' // eslint-disable-line camelcase
+  const previous = rawFile.previous_filename || (rawFile.status === 'modified' && rawFile.filename) || '' // eslint-disable-line camelcase
   const {slug: previousSlug} = extractNames(previous, team)
   if (!previousSlug || previousSlug !== slug) {
     return db.insight.findOne({slug}, '_id').lean().then(i => !i)

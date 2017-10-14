9e5375b575dd11fdfaf5b1ce3aef232aab613c5d
Cătălin Buruiană <catalin@enki.com>
2017-10-14 16:14:16 +0200

    get file to same version

--- tests/rules/author.js
+++ tests/rules/author.js
@@ -2,6 +2,6 @@ export const context = 'author'
 export const description = 'author'
 export const link = 'https://enkidevs.github.io/guidelines/Insights-guidelines.html#author-m'
 
-export default function ({data: {author}, type}, {db}) {
+export default function ({data: {author}, type}) {
   if (type !== 'insight') return true
   return typeof author === 'string' && !!author.length
